ef78215c62ac5ddf993eb37473d608127a8c6fe0
greendy5 <pyqt@yandex.ru>
2016-06-08 19:45:03 +0500

    Was changed the control in menu and choice submenu, was optimized a bit. I took some comments

--- Maze/Main.cpp
+++ Maze/Main.cpp
@@ -1,6 +1,6 @@
 /*
 The Maze.
-This is a game where you have to go through the maze and collect the items on the map.
+This is a game where you have to go through the maze and collect all items on the map.
 
 1.Function Loop creates functions call, the generation of the world,
 the game, the playing field, as the player will not pass all the levels;
@@ -9,7 +9,7 @@ the game, the playing field, as the player will not pass all the levels;
 depending of type of the transferred loop variable level number. It translates from char to int;
 
 3.WindowControl creates a map on the screen of the game,
-turn handles and sends the value to the function Data(4);
+turn handles and sends the value to the function "Data";
 
 4.Function Data implements the data collection made by the player in the map array
 and the collected items and the number of lives,
@@ -22,8 +22,9 @@ the number of levels in which to create the conditions to continue or interrupt 
 7.Description elements of array:
 0 - Path
 1 - Wall
-2 - Spawnpoint(fixed)
+2 - Spawnpoint ("player")
 3 - Exit
+4 - Items ("food")
 
 Made by greendy5.
 */
@@ -34,7 +35,7 @@ Made by greendy5.
 #ifdef _WIN32 || _WIN64
 #include <windows.h>
 #endif
-
+// function, which erase screen, it can be used only in Windows OS
 void ClrScr()
 {
 #ifdef _WIN32 || _WIN64
@@ -62,100 +63,113 @@ void ClrScr()
 using namespace std;
 
 void Loop(unsigned short LevelNum, unsigned short Health);
-void GameOver(unsigned short LevelNum, unsigned short Health){
+void Menu(unsigned short Arrow);
 
-	system("cls");
-	cout << "\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t     Game Over!\n\n\n\n\n\n\n\n\n\n\n";
+void GameOver(unsigned short LevelNum, unsigned short Health, unsigned short Arrow){
+	
+	ClrScr(); // it cleans screen (it's stable analog of system("cls")
+	cout << "\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t     Game Over!\n\n\n\n\n\n";
 
-	if (Health < 1) { 
+	if (Health < 1) { // it stops game, if count of lives = 0
 		_getch();
-		exit(0); 
+		Menu(1);
 	}
 
-	cout << "\t\t\t\t Do you want restart?[y]/[n]";
-	cout << "\n\t\t\t\t\t";
+	cout << "\t\t\t\t Do you want restart?[y]/[n]";							// it can continue game, if player lost previous play
+	cout << (Arrow == 1 ? "\n\n\t\t\t\t ->" : "\n\n\t\t\t\t   ") << " Yes";
+	cout << (Arrow == 2 ? "\n\n\t\t\t\t ->" : "\n\n\t\t\t\t   ") << " No";
 
-	char choice;
-	cin >> choice;
-	switch (choice) {
-	case 'y': {
-		Health--;
-		Loop(LevelNum, Health);
+	unsigned short Category = _getch();										// it realise "Arrow control" to continue or quit to main menu
+
+	switch (Category) {
+	case 72: {
+		Arrow--;
+		if (Arrow < 1) Arrow = 2;
+		GameOver(LevelNum, Health, Arrow);
 	} break;
-	case 'n': exit(0); break;
-	case 't': {
-		unsigned short n;
-		cout << "\t\t\t\t Input Level Number: ";
-		cin >> n;
-		LevelNum = n;
-		Loop(LevelNum, Health);
+	case 80: {
+		Arrow++;
+		if (Arrow > 2) Arrow = 1;
+		GameOver(LevelNum, Health, Arrow);
+	} break;
+	case 13: {
+		switch (Arrow) {
+		case 1: {
+			LevelNum++;
+			Loop(LevelNum, Health);
+		}break;
+		case 2: {
+			Menu(1);
+		}break;
+		}
+	} break;
+	default: GameOver(LevelNum, Health, Arrow); break;						//prevents default keys to escape errors
 	}
-	default: GameOver(LevelNum, Health); break;
-	}
-
 }
 
-void Victory(unsigned short LevelNum, unsigned short Health){
+void Victory(unsigned short LevelNum, unsigned short Health, unsigned short Arrow){
+	ClrScr();
+	cout << "\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t     Victory!\n\n\n\n\n\n\n\t\t\t\t Do you want continue?[y]/[n]";
+	cout << (Arrow == 1 ? "\n\n\t\t\t\t ->" : "\n\n\t\t\t\t   ") << " Yes";
+	cout << (Arrow == 2 ? "\n\n\t\t\t\t ->" : "\n\n\t\t\t\t   ") << " No";
 
-	system("cls");
-	cout << "\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t     Victory!\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t Do you want continue?[y]/[n]";
-	cout << "\n\t\t\t\t\t";
+	unsigned short Category = _getch();										// "Arrow control"
 
-	char choice;
-	cin >> choice;
-
-	switch (choice) {
-	case 'y': {
-		LevelNum++;
-		Loop(LevelNum, Health);
+	switch (Category) {
+	case 72: {
+		Arrow--;
+		if (Arrow < 1) Arrow = 2;
+		Victory(LevelNum, Health, Arrow);
 	} break;
-
-	case 'n': exit(0); break;
-
-	case 't': {
-		unsigned short n;
-		cout << "\t\t\t\t Input Level Number: ";
-		cin >> n;
-		LevelNum = n;
-		Loop(LevelNum, Health);
+	case 80: {
+		Arrow++;
+		if (Arrow > 2) Arrow = 1;
+		Victory(LevelNum, Health, Arrow);
+	} break;
+	case 13: {
+		switch (Arrow) {
+		case 1: {
+			LevelNum++;
+			Loop(LevelNum, Health);
+		}break;
+		case 2: {
+			Menu(1);
+		}break;
+		}
+	} break;
+	default: Victory(LevelNum,Health, Arrow); break;
 	}
-	default: Victory(LevelNum, Health); break;
-	}
-
 }
 
-void Data(unsigned short Management, unsigned short StopGame, unsigned short Score, unsigned short Items, unsigned short LevelNum, unsigned short Health){
-	if (Management == 1) 
+void Data(unsigned short Management, unsigned short Score, unsigned short Items, unsigned short LevelNum, unsigned short Health){ // this function directs to function of "Win"(Victory) or "Lose"(GameOver) (Number == Number of Wall)
+	unsigned short Arrow = 1;					
+	if (Management == 1)								
 	{
-		GameOver(LevelNum, Health);
-		StopGame = 1;
+		GameOver(LevelNum, Health, Arrow);
+
 	}
-	if (Management == 3)
+	if (Management == 3)								//(DoorNum)
 	{
-		if (Score == Items)
+		if (Score == Items)								// If Player collects all items, he will win a level 
 		{
-			Victory(LevelNum, Health);
-			StopGame = 1;
+
+			Victory(LevelNum, Health, Arrow);			
 		}
 		else
 		{
-			GameOver(LevelNum, Health);
-			StopGame = 1;
+			GameOver(LevelNum, Health, Arrow);		   
 		}
 	}
-	Management = 2;
+	Management = 2;									   //(PlayerNum)
 }
 void WindowControl(unsigned short LevelNum, unsigned short Items, char LevelArray[24][24], unsigned short Health, unsigned short y, unsigned short x) {
 
 	unsigned short keysArrow;
 	unsigned short heart = 3;
 	unsigned short Score = 0;
-	unsigned short StopGame(0);
 
-	do {
+	for (;;) {
 
-		/*system("Cls");*/
-		/*ClrScr();*/
 		ClrScr();
 
 		for (unsigned short i = 0; i < 23; i++)
@@ -191,82 +205,46 @@ void WindowControl(unsigned short LevelNum, unsigned short Items, char LevelArra
 		keysArrow = _getch();
 		if (keysArrow == 0) keysArrow = _getch();
 		switch (keysArrow) {
-		case 80: {
+		case 80: { //Up
 			LevelArray[y][x] = 1;
 			y++;
 			if (LevelArray[y][x] == 4) {
 				Score++;
 			}
-			Data(LevelArray[y][x], StopGame, Score, Items, LevelNum, Health);
+			Data(LevelArray[y][x], Score, Items, LevelNum, Health);
 			LevelArray[y][x] = 2;
 		}break;
-		case 72: {
+		case 72: { //Down
 			LevelArray[y][x] = 1;
 			y--;
 			if (LevelArray[y][x] == 4) {
 				Score++;
 			}
-			Data(LevelArray[y][x], StopGame, Score, Items, LevelNum, Health);
+			Data(LevelArray[y][x], Score, Items, LevelNum, Health);
 			LevelArray[y][x] = 2;
 		}break;
-		case 77: {
+		case 77: { //Right
 			LevelArray[y][x] = 1;
 			x++;
 			if (LevelArray[y][x] == 4) {
 				Score++;
 			}
-			Data(LevelArray[y][x], StopGame, Score, Items, LevelNum, Health);
+			Data(LevelArray[y][x], Score, Items, LevelNum, Health);
 			LevelArray[y][x] = 2;
 		}break;
-		case 75: {
+		case 75: { //Left
 			LevelArray[y][x] = 1;
 			x--;
 			if (LevelArray[y][x] == 4) {
 				Score++;
 			}
-			Data(LevelArray[y][x], StopGame, Score, Items, LevelNum, Health);
+			Data(LevelArray[y][x], Score, Items, LevelNum, Health);
 			LevelArray[y][x] = 2;
 		}break;
-		/*case 107: {
-			LevelArray[y][x] = 0;
-			y++;
-			if (LevelArray[y][x] == 4) {
-				Score++;
-			}
-			Data(LevelArray[y][x], StopGame, Score, Items, LevelNum, Health);
-			LevelArray[y][x] = 2;
-		}break;
-		case 105: {
-			LevelArray[y][x] = 0;
-			y--;
-			if (LevelArray[y][x] == 4) {
-				Score++;
-			}
-			Data(LevelArray[y][x], StopGame, Score, Items, LevelNum, Health);
-			LevelArray[y][x] = 2;
-		}break;
-		case 108: {
-			LevelArray[y][x] = 0;
-			x++;
-			if (LevelArray[y][x] == 4) {
-				Score++;
-			}
-			Data(LevelArray[y][x], StopGame, Score, Items, LevelNum, Health);
-			LevelArray[y][x] = 2;
-		}break;
-		case 106: {
-			LevelArray[y][x] = 0;
-			x--;
-			if (LevelArray[y][x] == 4) {
-				Score++;
-			}
-			Data(LevelArray[y][x], StopGame, Score, Items, LevelNum, Health);
-			LevelArray[y][x] = 2;
-		}break;*/
 
 		default: WindowControl(LevelNum, Items, LevelArray, Health, y, x); break;
 		}
-	} while (true);
+	} 
 }
 void LevelGenerator(unsigned short LevelNum, unsigned short Health) {
 
@@ -274,21 +252,23 @@ void LevelGenerator(unsigned short LevelNum, unsigned short Health) {
 	unsigned short Items = 0;
 	unsigned short y = 0;
 	unsigned short x = 0;
+
 	ifstream InputLevel1("level1.txt");
 	ifstream InputLevel3("level2.txt");
 	ifstream InputLevel2("level3.txt");
 	ifstream InputLevel4("level4.txt");
 	ifstream InputLevel5("level5.txt");
+
 	switch (LevelNum) {
 	case 1:
 	{
 		for (int i = 0; i < 24; i++) {
 			for (int j = 0; j < 24; j++) {
-				InputLevel1.get(LevelArray[i][j]);
-				LevelArray[i][j] = LevelArray[i][j] - '0';
-				if (LevelArray[i][j] == 4) Items++;
+				InputLevel1.get(LevelArray[i][j]);				//level 1
+				LevelArray[i][j] = LevelArray[i][j] - '0';		//Translate char into integer
+				if (LevelArray[i][j] == 4) Items++;				//Sum of "Items" in file
 				if (LevelArray[i][j] == 2) {
-					y = i;
+					y = i;										//Player's coordinates 
 					x = j;
 				}
 			}
@@ -297,7 +277,7 @@ void LevelGenerator(unsigned short LevelNum, unsigned short Health) {
 	case 2:
 	{
 		for (int i = 0; i < 24; i++) {
-			for (int j = 0; j < 24; j++) {
+			for (int j = 0; j < 24; j++) {				//level 2
 				InputLevel2.get(LevelArray[i][j]);
 				LevelArray[i][j] = LevelArray[i][j] - '0';
 				if (LevelArray[i][j] == 4) Items++;
@@ -311,7 +291,7 @@ void LevelGenerator(unsigned short LevelNum, unsigned short Health) {
 	case 3:
 	{
 		for (int i = 0; i < 24; i++) {
-			for (int j = 0; j < 24; j++) {
+			for (int j = 0; j < 24; j++) {				//level 3
 				InputLevel3.get(LevelArray[i][j]);
 				LevelArray[i][j] = LevelArray[i][j] - '0';
 				if (LevelArray[i][j] == 4) Items++;
@@ -326,7 +306,7 @@ void LevelGenerator(unsigned short LevelNum, unsigned short Health) {
 	{
 		for (int i = 0; i < 24; i++) {
 			for (int j = 0; j < 24; j++) {
-				InputLevel4.get(LevelArray[i][j]);
+				InputLevel4.get(LevelArray[i][j]);		//level 4
 				LevelArray[i][j] = LevelArray[i][j] - '0';
 				if (LevelArray[i][j] == 4) Items++;
 				if (LevelArray[i][j] == 2) {
@@ -339,37 +319,96 @@ void LevelGenerator(unsigned short LevelNum, unsigned short Health) {
 	case 5:
 	{
 		for (int i = 0; i < 24; i++) {
-			for (int j = 0; j < 24; j++) {
+			for (int j = 0; j < 24; j++) {				//level 5
 				InputLevel5.get(LevelArray[i][j]);
 				LevelArray[i][j] = LevelArray[i][j] - '0';
 				if (LevelArray[i][j] == 4) Items++;
 				if (LevelArray[i][j] == 2) {
-					y = i;
+					y = i;								//Player's coordinates 
 					x = j;
 				}
 			}
 		}
 	} break;
 	}
-
 	WindowControl(LevelNum, Items, LevelArray, Health, y, x);
 }
+
 void Loop(unsigned short LevelNum, unsigned short Health) {
-	while (LevelNum<6) {
-		LevelGenerator(LevelNum, Health);
+	while (LevelNum<6) {								//Number of cycles 
+		LevelGenerator(LevelNum, Health);				//Generate level
 	} if (LevelNum == 6) {
-		system("cls");
+		ClrScr();
 		cout << "\n\n\n\n\n\n\t\t\t\t Well done!";
 		_getch();
 		exit(0);
 	}
 }
-void Menu() {
 
-	unsigned short MenuNum;
-	unsigned short Wall = 176;
-	unsigned short Door = 177;
+void SubMenu(unsigned short Arrow) {
+		ClrScr();
+		cout << "\n\n\t\t\t Choose level: "; // The second point of Menu (choosing a level)
 
+		cout << (Arrow == 1 ? "\n\n\t\t\t ->" : "\n\n\t\t\t   ") << " Level 1";
+		cout << (Arrow == 2 ? "\n\n\t\t\t ->" : "\n\n\t\t\t   ") << " Level 2";
+		cout << (Arrow == 3 ? "\n\n\t\t\t ->" : "\n\n\t\t\t   ") << " Level 3";
+		cout << (Arrow == 4 ? "\n\n\t\t\t ->" : "\n\n\t\t\t   ") << " Level 4";
+		cout << (Arrow == 5 ? "\n\n\t\t\t ->" : "\n\n\t\t\t   ") << " Level 5";
+		cout << (Arrow == 6 ? "\n\n\t\t\t ->" : "\n\n\t\t\t   ") << " Exit to main menu";
+
+		unsigned short Category = _getch();
+
+		switch (Category) {
+		case 72: {
+			Arrow--;
+			if (Arrow < 1) Arrow = 6;
+			SubMenu(Arrow);
+		} break;
+		case 80: {
+			Arrow++;
+			if (Arrow > 6) Arrow = 1;
+			SubMenu(Arrow);
+		} break;
+		case 13: {
+			switch (Arrow) {			//choosing a level
+			case 1: {
+				unsigned short LevelNum = 1;
+				unsigned short Health = 3;
+				Loop(LevelNum, Health);
+			}break;
+			case 2: {
+				unsigned short LevelNum = 2;
+				unsigned short Health = 3;
+				Loop(LevelNum, Health);
+			}break;
+			case 3: {
+				unsigned short LevelNum = 3;
+				unsigned short Health = 3;
+				Loop(LevelNum, Health);
+			}break;
+			case 4: {
+				unsigned short LevelNum = 4;
+				unsigned short Health = 3;
+				Loop(LevelNum, Health);
+			}break;
+			case 5: {
+				unsigned short LevelNum = 5;
+				unsigned short Health = 3;
+				Loop(LevelNum, Health);
+			}break;
+			case 6: {
+				Arrow = 1;
+				Menu(Arrow);
+			}break;
+		}
+		} break;
+		default: SubMenu(Arrow); break;
+		}
+	}
+
+void Menu(unsigned short Arrow) { // Starting menu
+	ClrScr();
+	unsigned short l = 178;
 	int IntroArray[5][23] = {
 		{ 1,0,0,0,1,0,0,1,1,1,0,0,1,1,1,1,1,0,1,1,1,1,1, },
 		{ 1,1,0,1,1,0,1,0,0,0,1,0,1,0,0,1,0,0,1,0,0,0,0, },
@@ -382,54 +421,63 @@ void Menu() {
 			if (IntroArray[i][j] == 0) {
 				cout << "  ";
 			} else {
-				unsigned short l = 178;
 				cout << (char)l;
 				cout << (char)l;
 			}
 		}
 		cout << "\n\t\t\t";
 	}
-	cout << "\n\t\t\tGame menu:";
-	cout << "\n\n\t\t\t 1. Play game";
-	cout << "\n\t\t\t 2. Choose level";
-	cout << "\n\t\t\t 3. Game rules";
-	cout << "\n\t\t\t 4. Quit\n\t\t\t";
 
-	cin >> MenuNum;
-	switch (MenuNum) {
-	case 1: {
-		unsigned short LevelNum = 1;
-		unsigned short Health = 3;
-		Loop(LevelNum, Health);
+	cout <<(Arrow == 1 ? "\n\n\t\t\t ->" : "\n\n\t\t\t   ") <<" Play";
+	cout <<(Arrow == 2 ? "\n\n\t\t\t ->" : "\n\n\t\t\t   ") <<" Choose level";
+	cout <<(Arrow == 3 ? "\n\n\t\t\t ->" : "\n\n\t\t\t   ") <<" Game rules";
+	cout <<(Arrow == 4 ? "\n\n\t\t\t ->" : "\n\n\t\t\t   ") <<" Quit\n\t\t\t  ";
+	unsigned short Category = _getch();
+
+	switch (Category) {
+	case 72: {
+		Arrow--;
+		if (Arrow < 1) Arrow = 4;
+		Menu(Arrow);
 	} break;
-	case 2: {
-		system("cls");
-		unsigned short LevelNum;
-		unsigned short Health = 3;
-		cout << "\n\n\t\tChoose level (1-5)";
-		cin >> LevelNum;
-		Loop(LevelNum, Health);
-	}break;
-	case 3: {
-		system("cls");
-		cout << "\n\n\t\t GAME RULES \n \t\t 1) Player(You):  [] \n\t\t 2) Items(You must collect all of them to escape level):  <> \n\t\t 3) Walls(You shouldn`t take collision with it):  "
-			<< (char)Wall << (char)Wall << "\n\t\t 4) Door(If You want to win, You should go there):  " << (char)Door << (char)Door << "\n\t\t 5) You have 3 lifes" << "\n\n\n\t\t Press some key to escape..." << endl;
-		_getch();
-		system("cls");
-		Menu();
-	}break;
-	case 4: {
-		exit(0);
-	}break;
-	default: Menu(); break;
+	case 80: {
+		Arrow++;
+		if (Arrow > 4) Arrow = 1;
+		Menu(Arrow);
+	} break;
+	case 13: {
+		switch (Arrow) {
+		case 1: {			//start game
+			unsigned short LevelNum = 1;
+			unsigned short Health = 3;
+			Loop(LevelNum, Health);
+		} break;
+		case 2: {			//choosing a level
+			Arrow = 1;
+			SubMenu(Arrow);
+		}break;
+		case 3: {			//game rules
+			unsigned short Wall = 176;
+			unsigned short Door = 177;
+			ClrScr();
+			cout << "\n\n\t\t GAME RULES \n \t\t 1) Player(You):  [] \n\t\t 2) Items(You must collect all of them to escape level):  <> \n\t\t 3) Walls(You shouldn`t take collision with it):  "
+				<< (char)Wall << (char)Wall << "\n\t\t 4) Door(If You want to win, You should go there):  " << (char)Door << (char)Door << "\n\t\t 5) You have 3 lifes" << "\n\n\n\t\t Press some key to escape..." << endl;
+			_getch();
+			Menu(Arrow);
+		}break;
+		case 4: {			//quit
+			exit(0);
+		}break;
+		}
+	} break;
+	default: Menu(Arrow); break;
 	}
-
 
 }
 
 int main() {
-
-	Menu();
+	unsigned short Arrow = 1;
+	Menu(Arrow);
 	return 0;
 
 }
